0580f861c007595cbf950cbeff895570a03d2e8c
danmuck <danmuck426@gmail.com>
2021-09-24 23:19:11 -0400

    formatting

--- debate_project/views.py
+++ debate_project/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Evidence
