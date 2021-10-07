d78b68e1bae8af98cab78e9febdd5a0d3bd63d72
danmuck <danmuck426@gmail.com>
2021-10-07 13:00:30 -0400

    test

--- debate_project/models.py
+++ debate_project/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 # Create your models here.
 
 class Evidence(models.Model):
-    title       = models.CharField(max_length=500) #max length req
+    title       = models.CharField(max_length=1000) #max length req
     description = models.TextField(blank=True, null=True)
     speaker     = models.CharField(max_length=50, null=True, default='n/a')
     source      = models.URLField(max_length=200, default='n/a', null=True)
