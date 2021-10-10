ff2b3818dbe41037ea957614e321de0159c3d529
danmuck <danmuck426@gmail.com>
2021-10-09 20:05:59 -0400

    project nav

--- postal/views.py
+++ postal/views.py
@@ -20,7 +20,7 @@ class ArticleListView(ListView):
     template_name = "postal/article_list.html"
 
 class ArticleDebateView(ListView):
-    queryset = Article.objects.all().filter(title__startswith='debate').order_by('pk') # <app_name>/<model_name>_list.html #this is the default path it looks for template 
+    queryset = Article.objects.get(title__contains='debate').order_by('pk') # <app_name>/<model_name>_list.html #this is the default path it looks for template 
     model = Article
     template_name = "postal/article_debate.html"
 
